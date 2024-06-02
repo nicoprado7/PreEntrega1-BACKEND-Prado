a5b846bb9a5dd2fa55b6adc7a6055c510f6e7282
Nicolás Prado <nicoprado7@hotmail.com>
2024-06-02 16:00:45 -0300

    detalles

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
+app.use(express.json());
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+const PORT = 8080;
+const HOST = 'localhost';
+
+app.listen(PORT, HOST, () => {
+  console.log(`Ejecutandose en http://${HOST}:${PORT}`);
+});
